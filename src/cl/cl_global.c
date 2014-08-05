3255087b107f3d09e00dd71023afad53dc7991de
Nyx0uf <benjgodard@me.com>
2014-08-05 21:15:02 +0200

    OpenCL: fix build on Linux
    - add a debug log to see best vector size
    - change some vars name

--- src/cl/cl_global.c
+++ src/cl/cl_global.c
@@ -13,34 +13,35 @@ bool nyx_cl_init(void)
 {
 	if (__is_init)
 		return true;
-#if 1
-	static cl_platform_id __platforms[10] = {NULL};
-	static cl_uint __num_platforms = 0;
-	cl_int err = clGetPlatformIDs(10, __platforms, &__num_platforms);
+
+#ifdef NYX_DEBUG
+	cl_platform_id platforms[10] = {NULL};
+	cl_uint num_platforms = 0;
+	cl_int err = clGetPlatformIDs(10, platforms, &num_platforms);
 	if (err != CL_SUCCESS)
 	{
 		NYX_ERRLOG("[!] Error: Failed to get platforms (%d)\n", err);
 		return false;
 	}
-#ifdef NYX_DEBUG
-	NYX_DLOG("%d OpenCL platform%s found:\n", __num_platforms, (__num_platforms > 1) ? "s" : "");
-	for (cl_uint i = 0; i < __num_platforms; i++)
+
+	NYX_DLOG("%d OpenCL platform%s found:\n", num_platforms, (num_platforms > 1) ? "s" : "");
+	for (cl_uint i = 0; i < num_platforms; i++)
 	{
 		char buffer[1024];
 		NYX_DLOG("-- platform %d --\n", (i + 1));
-		clGetPlatformInfo(__platforms[i], CL_PLATFORM_PROFILE, 1024, buffer, NULL);
+		clGetPlatformInfo(platforms[i], CL_PLATFORM_PROFILE, 1024, buffer, NULL);
 		NYX_DLOG(" PROFILE = %s\n", buffer);
-		clGetPlatformInfo(__platforms[i], CL_PLATFORM_VERSION, 1024, buffer, NULL);
+		clGetPlatformInfo(platforms[i], CL_PLATFORM_VERSION, 1024, buffer, NULL);
 		NYX_DLOG(" VERSION = %s\n", buffer);
-		clGetPlatformInfo(__platforms[i], CL_PLATFORM_NAME, 1024, buffer, NULL);
+		clGetPlatformInfo(platforms[i], CL_PLATFORM_NAME, 1024, buffer, NULL);
 		NYX_DLOG(" NAME = %s\n", buffer);
-		clGetPlatformInfo(__platforms[i], CL_PLATFORM_VENDOR, 1024, buffer, NULL);
+		clGetPlatformInfo(platforms[i], CL_PLATFORM_VENDOR, 1024, buffer, NULL);
 		NYX_DLOG(" VENDOR = %s\n", buffer);
-		clGetPlatformInfo(__platforms[i], CL_PLATFORM_EXTENSIONS, 1024, buffer, NULL);
+		clGetPlatformInfo(platforms[i], CL_PLATFORM_EXTENSIONS, 1024, buffer, NULL);
 		NYX_DLOG(" EXTENSIONS = %s\n", buffer);
 	}
-#endif /* NYX_DEBUG */
 #endif
+
 	// connect to a compute device
 	err = clGetDeviceIDs(NULL, CL_DEVICE_TYPE_GPU, 1, &__device_id, NULL);
 	if (err != CL_SUCCESS)
@@ -50,7 +51,7 @@ bool nyx_cl_init(void)
 	}
 
 	// create a compute context
-#ifdef NYX_DEBUG
+#if ((defined NYX_DEBUG) && (defined __APPLE__))
 	__context = clCreateContext(0, 1, &__device_id, NULL, clLogMessagesToStdoutAPPLE, &err);
 #else
 	__context = clCreateContext(0, 1, &__device_id, NULL, NULL, &err);
@@ -75,6 +76,7 @@ bool nyx_cl_init(void)
 	// get some global params
 	clGetDeviceInfo(__device_id, CL_DEVICE_PREFERRED_VECTOR_WIDTH_INT, sizeof(cl_uint), &(__vector_width[0]), NULL);
 	clGetDeviceInfo(__device_id, CL_DEVICE_PREFERRED_VECTOR_WIDTH_FLOAT, sizeof(cl_uint), &(__vector_width[1]), NULL);
+	NYX_DLOG("[+] CL_DEVICE_PREFERRED_VECTOR_WIDTH_INT : %d\n[+] CL_DEVICE_PREFERRED_VECTOR_WIDTH_FLOAT : %d\n", __vector_width[0], __vector_width[1]);
 
 	__is_init = true;
 	return true;
@@ -109,11 +111,19 @@ cl_command_queue nyx_cl_get_commandqueue(void)
 cl_uint nyx_cl_get_int_vector_width(void)
 {
 	//return 1;
+	//return 2;
+	//return 4;
+	//return 8;
+	//return 16;
 	return __vector_width[0];
 }
 
 cl_uint nyx_cl_get_float_vector_width(void)
 {
 	//return 1;
+	//return 2;
+	//return 4;
+	//return 8;
+	//return 16;
 	return __vector_width[1];
 }
